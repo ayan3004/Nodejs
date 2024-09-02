22619b646dbf09603064337d99f57adcac8fabc5
ayan3004 <ayansangani3004@gmail.com>
2024-09-02 18:51:05 +0530

    webp

---
+++ routes web page/controllers/web.js
@@ -0,0 +1,10 @@
+module.exports.index=(req,res)=>{
+ res.render("index")
+}
+module.exports.home=(req,res)=>{
+    res.render("home")
+   }
+module.exports.about=(req,res)=>{
+    res.render("about")
+   }
+   
