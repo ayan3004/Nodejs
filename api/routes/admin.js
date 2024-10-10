8f1d68cb981ca853673484c488875b658204323f
ayan3004 <ayansangani3004@gmail.com>
2024-10-10 17:48:51 +0530

    api postman

---
+++ api/routes/admin.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = express.Router();
+const adminctl = require("../controller/adminctl");
+
+const multer = require("multer");
+const Storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'uploades/admin')
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() )
+    }
+})
+
+const upload = multer({storage : Storage}).single("image")
+
+routes.post("/addadmin",upload,adminctl.addadmin)
+
+
+module.exports = routes;    
